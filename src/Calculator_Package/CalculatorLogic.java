96afe683d3f290931f67b615af63cccb27be229b
Otir <45825931+otirmesilati@users.noreply.github.com>
2024-05-08 15:09:11 +0300

    Build 0.1
    -> Comment Format: Constraints &
    Dependencies
    
    -> Design: Non.
    
    -> Implementation: Non.

--- src/Calculator_Package/CalculatorLogic.java
+++ src/Calculator_Package/CalculatorLogic.java
@@ -1,3 +1,20 @@
+/*  &&&&&&&&&&&&&&&&&&&&&&&&&&&&&   Project : Java Calculator  &&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+    &&&&&&&&&&&&&&&&&&&&&&&&&&&&&   LANGUAGE: Java             &&&&&&&&&&&&&&&&&&&&&&&&&&&&&  
+    &&&&&&&&&&&&&&&&&&&&&&&&&&&&&   Date: 08/05/2024           &&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+    &&&&&&&&&&&&&&&&&&&&&&&&&&&&&   Author: Otir Mesilati      &&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+
+-> Functions: 0
+
+Function Specifications:
+
+1. ?: ?
+   IN:   ?
+   OUT:  ?
+
+->> Classes: ?
+
+*/
+
 package Calculator_Package;
 
 public class CalculatorLogic {

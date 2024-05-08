18d15ac8d98e33e54aac4e3120855c456413fcac
Otir <otirmesi@gmail.com>
2024-05-08 17:09:20 +0300

    Build 0.5
    > Comment Format: Non.
    
    -> Design: Non.
    
    -> Implementation:
    Back -
    
    1. multiply and division function attachments

--- src/Calculator_Package/CalculatorLogic.java
+++ src/Calculator_Package/CalculatorLogic.java
@@ -39,6 +39,9 @@ public class CalculatorLogic {
   
   public static int subtractTwoNumbers(int minuend, int subtrahend) {return minuend - subtrahend;}
 
+  public static int multiplyTwoNumbers(int multiplicand, int multiplier) {return multiplicand * multiplier;}
+
+  public static int divideTwoNumbers(int dividend, int divisor) {return dividend / divisor;}
 
   public static void main(String [] args) 
   {
@@ -46,6 +49,8 @@ public class CalculatorLogic {
     System.out.println("Hello :-) ");
     System.out.println(addTwoNumbers(1,3));
     System.out.println(subtractTwoNumbers(4,2));
+    System.out.println(multiplyTwoNumbers(3,4));
+    System.out.println(divideTwoNumbers(8,4));
   }
 
 }
